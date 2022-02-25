89806866712370f3dd108b541bb263d2b1fb7301
LuisVilla117 <zs18014494@estudiantes.uv.mx>
2022-02-25 16:30:37 -0600

    Subir code

---
+++ soap-test/Services/Operaciones.cs
@@ -0,0 +1,19 @@
+using System;
+using WSDL.mensajes;
+
+namespace WSDL.operaciones
+{
+    public class Operaciones : Mensajes
+    {
+        public string Saludar(string nombre)
+        {
+            string msj = "Hola " + nombre + ", te saluda Luis Villa";
+            return msj;
+        }
+
+        public string Mostrar(int id)
+        {
+            return "x";
+        }
+    }
+}
