1ba11f6075554b5678ebab1e817055bf0c195c26
LuisVilla117 <zs18014494@estudiantes.uv.mx>
2022-03-11 16:00:12 -0600

    dockerfile

---
+++ calculadora-pojo/calculadora.java
@@ -0,0 +1,17 @@
+public class calculadora {
+    public int suma (int a, int b) {
+        return a + b;
+    }
+    
+    public int resta (int a, int b) {
+        return a - b;
+    }
+    
+    public int multiplcia (int a, int b) {
+        return a * b;
+    }
+    
+    public int divide (int a, int b) {
+        return a / b;
+    }
+}
